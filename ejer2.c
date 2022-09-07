d4e86c17421cc5d3f8e9aba2fa121ddf957bae15
Gustavo Sotres <gus.sotres@gmail.com>
2022-09-06 20:51:43 -0500

    Exercise 2 finished

--- ejer2.c
+++ ejer2.c
@@ -3,16 +3,29 @@
 #include <unistd.h>
 #include <math.h>
 #include <sys/time.h>
+#include <pthread.h>
 
 #define ITERS 1000000000
+#define N_THREADS 4
+
+pthread_mutex_t mutex;
+double sum = 0.0;
+
+struct log_args
+{
+	float x;
+	int start;
+	int end;
+};
+
+void *paralelized_log(void *args);
 
 int main()
 {
 	clock_t start_ts, stop_ts;
 	double elapsed_time;
-	int n;
-	double sum = 0.0;
 	float x;
+	pthread_t tid[N_THREADS];
 
 	printf("Calcular el logaritmo natural de 1+x\n");
 	printf("Dame el valor de x :");
@@ -20,10 +33,18 @@ int main()
 
 	start_ts = clock(); // Tiempo inicial
 
-	for (n = 1; n < ITERS; n++)
+	pthread_mutex_init(&mutex, NULL);
+	for (int t = 0; t < N_THREADS; ++t)
 	{
-		double power = pow(-1, n + 1) * pow(x, n) / n; // Separar cálculo para hacer paralelización
-		sum = sum + power;
+		struct log_args p;
+		p.x = x;
+		p.start = (ITERS / N_THREADS * t) + 1;
+		p.end = (ITERS / N_THREADS * (t + 1)) + 1;
+		pthread_create(&tid[t], NULL, paralelized_log, &p);
+	}
+	for (int t = 0; t < N_THREADS; ++t)
+	{
+		pthread_join(tid[t], NULL);
 	}
 
 	stop_ts = clock(); // Tiempo final
@@ -32,4 +53,19 @@ int main()
 	printf("Resultado = %.4lf\n", sum);
 	printf("------------------------------\n");
 	printf("TIEMPO TOTAL, %f segundos\n", elapsed_time / 1000);
+}
+
+void *paralelized_log(void *args)
+{
+	struct log_args cast_args = *((struct log_args *)args);
+	double local_sum = 0.0;
+
+	for (int n = cast_args.start; n < cast_args.end; n++)
+	{
+		double power = pow(-1, n + 1) * pow(cast_args.x, n) / n; // Separar cálculo para hacer paralelización
+		local_sum += power;
+	}
+	pthread_mutex_lock(&mutex);
+	sum += local_sum;
+	pthread_mutex_unlock(&mutex);
 }
